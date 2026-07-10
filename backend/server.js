8e4ad74339020cb4772e3dc287952a709bcf0ed5
Niraj Kumar <niraj@origincoretech.com>
2026-07-10 16:22:37 +0530

    all

--- backend/server.js
+++ backend/server.js
@@ -102,7 +102,7 @@ const io = new Server(server, {
   cors: {
     origin: [
       "http://localhost:5173",
-      "https://real-time-inventory-updates.vercel.app/api",
+      "https://real-time-inventory-updates.vercel.app",
     ],
     methods: ["GET", "POST"],
     credentials: true,
