5c8ae5024cb6d1f490a2caf0e5a42ee60d77ae29
Niraj Kumar <niraj@origincoretech.com>
2026-07-10 16:29:54 +0530

    all

--- frontend/src/socket/socket.js
+++ frontend/src/socket/socket.js
@@ -1,5 +1,14 @@
+// import { io } from "socket.io-client";
+
+// const socket = io("https://realtime-inventory-updates.onrender.com/api");
+
+// export default socket;
+
+
 import { io } from "socket.io-client";
 
-const socket = io("https://realtime-inventory-updates.onrender.com/api");
+const socket = io("https://realtime-inventory-updates.onrender.com", {
+  transports: ["websocket", "polling"],
+});
 
 export default socket;
